30ea0a0d8db2e26ca8948c83fbb05f138608ad70
tirtapir <tirtarumy14@gmail.com>
2024-06-29 21:59:21 +0700

    Modelling

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -19,7 +19,7 @@ class DataTransformationConfig:
     preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
 
 class DataTransformation:
-    def __init__(self):
+    def __init__(self):  
         self.data_transformation_config=DataTransformationConfig()
 
     def get_data_transformer_object(self):
